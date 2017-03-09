b93f9c9c16d298e2ffdf557c7d305231af073778
dubech <franck.dubech@hotmail.com>
2017-03-09 12:00:59 +0100

    ajout de la fonction division

--- js/app.js
+++ js/app.js
@@ -9,7 +9,23 @@ substaction = function(a, b) {
 	console.log(a - b);
 };
 substaction(10, 5);
+
 multiplication = function(a, b) {
 	console.log(a * b);
 };
 multiplication(20, 5);
+
+division = function(a, b) {
+
+	if(b>0) {
+		console.log(a / b);
+	}
+	
+	
+	else{
+		console.log("Can't divide by 0");
+	}
+
+};
+division(10, 0);
+
